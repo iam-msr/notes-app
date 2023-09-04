ec2870b2f3f2f02e740b80e2a4a7e9ab9506fe36
iam-msr <sriram.mudragada4269+github@gmail.com>
2023-09-04 22:20:45 +0530

    added files

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import FeedIcon from '@mui/icons-material/Feed';
+
+function Header()
+{
+    return (
+        <header>
+            <h1>
+            <FeedIcon/>
+            Keeper
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
